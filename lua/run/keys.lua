ab0abeb9a562d7098911c4f873020f2acd3776ea
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-03-06 15:41:19 -0400

    add keybinds

--- lua/run/keys.lua
+++ lua/run/keys.lua
@@ -1,2 +1,27 @@
 --- Set keybindings
 local act = wez.action
+
+local c = wez.configuration
+
+c.leader = {
+  key = " ",
+  mods = "CTRL",
+  timeout_milliseconds = 1000,
+}
+
+c.keys = c.keys and c.keys or {}
+
+local function bind(keybinding)
+  table.insert(c.keys, keybinding)
+end
+
+-- This is a test for keybinds with notifications
+bind {
+  key = "t",
+  mods = "LEADER",
+  action = wez.action_callback(function(win, pane)
+    local msg =  'Toasty!'
+    win:toast_notification('wezterm',msg, nil, 4000)
+    wez.log_info(msg)
+  end)
+}
