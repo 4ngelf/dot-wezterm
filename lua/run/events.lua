e776859c0fd6a2d507c5cb818fea61307e99e59b
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-03-07 14:27:27 -0400

    format with stylua

--- lua/run/events.lua
+++ lua/run/events.lua
@@ -2,7 +2,7 @@
 local mux = wez.mux
 
 local first_gui_startup = true
-wez.on('gui-startup', function(cmd)
+wez.on("gui-startup", function(cmd)
   if first_gui_startup then
     local tab, pane, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
--- lua/run/keys.lua
+++ lua/run/keys.lua
@@ -20,8 +20,8 @@ bind {
   key = "t",
   mods = "LEADER",
   action = wez.action_callback(function(win, pane)
-    local msg =  'Toasty!'
-    win:toast_notification('wezterm',msg, nil, 4000)
+    local msg = "Toasty!"
+    win:toast_notification("wezterm", msg, nil, 4000)
     wez.log_info(msg)
-  end)
+  end),
 }
